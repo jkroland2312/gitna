87b3c5d403594ef0ce411fad7ecd9d2adc4710db
jkroland2312 <50861445+jkroland2312@users.noreply.github.com>
2020-09-16 14:59:05 +0700

    quiz2

---
+++ js/quiz2.js
@@ -0,0 +1,15 @@
+for (let i=1; i<= 20 ; i+=1) {
+    console.log(i)
+}
+
+for (let j=1; j<= 20 ; j++) {
+    if( j%2 == 0){
+        console.log(j)
+    }
+}
+
+let result1 = [-1, 2, 3, 56, 6, -93, 0, 4].map(el => el*2)
+console.log(result1)
+
+let result2 = [10, 13, 44, 55, 89, 0,12,1 ].filter(el => el%2 === 0)
+console.log(result2)
