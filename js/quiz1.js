03ffe578bc1d55fa18c2617720c2a5c887224feb
jkroland2312 <50861445+jkroland2312@users.noreply.github.com>
2020-09-16 13:50:48 +0700

    quiz1

---
+++ js/quiz1.js
@@ -0,0 +1,11 @@
+const a = {
+    firstname : "Groovy",
+    lastname : "bot",
+    age : 21,
+    nickname : "boyvy",
+    hobby : "play computer games",
+    Tel : "0924742418",
+
+}
+
+console.log(a)
