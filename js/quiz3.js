7652963bf58dcc5fee4739a85d1f3d8c639a2744
jkroland2312 <50861445+jkroland2312@users.noreply.github.com>
2020-09-16 15:30:44 +0700

    Update quiz3.js

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,7 +1,16 @@
-let x = (a) 
-    if(a > 80) {
-        a = 'A'
-    }
-    else (a <= 80) {
-        a = 'B'
-    }
+const calculateGrade = (point) => {
+    let garde = null
+    if(point >= 81 && point <= 100) {
+        console.log("A")
+    } else if (point >= 71 && point <= 80){
+        console.log("B")
+    } else if (point >= 61 && point <= 70){
+        console.log("C")
+    } else if (point >= 51 && point <= 60){
+        console.log("D")
+    } else if (point >= 0 && point <= 50){
+        console.log("F")
+    } 
+}
+
+console.log(calculateGrade(55))
